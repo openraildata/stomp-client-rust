127a0d8166ab2a6b75fa50fc4342e39ce3473da3
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-06 21:07:31 +0100

    chore: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+//! Contribution to OpenRailData, for a Rust NROD/NRDP client.
+#![deny(
+    warnings,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::cargo,
+    clippy::pedantic,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
+
+fn main() {
+    unimplemented!()
+}
